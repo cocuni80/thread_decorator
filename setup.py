2059c5fbe526f518a404f7d9ac9065e751b6a5d7
Jorge Riveros <christian.riveros@outlook.com>
2021-07-18 23:58:08 -0500

    ok

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = ['requests']
+
+setuptools.setup(
+    name="thread_decorator",
+    version="0.1",
+    author="Jorge Riveros",
+    author_email="christian.riveros@outlook.com",
+    license='MIT',
+    description='A Python package for Multithread processing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cocuni80/thread_decorator",
+    packages=['thread_decorator'],
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
